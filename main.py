5788a8b0bd7cb28e2aa8ca827130809ea48316c7
Pavel <caster977@gmail.com>
2021-02-26 17:29:46 +0200

    init

---
+++ main.py
@@ -0,0 +1,40 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from xvfbwrapper import Xvfb
+import urllib, json
+
+import http.server
+import socketserver
+
+PORT = 8003
+
+
+class MyServer(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        parsed_path = urllib.parse.urlsplit(self.path)
+        query = urllib.parse.parse_qs(parsed_path.query)
+        link = query['url'][0]
+
+        driver = webdriver.Chrome('./chromedriver')
+        driver.get(
+            link)
+        source = driver.page_source
+        driver.quit()
+        self.send_response(200)
+        self.send_header("Content-type", "application/json")
+        self.end_headers()
+
+        self.wfile.write(bytes(source, "utf8"))
+
+
+Handler = MyServer
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
+
+# sudo apt-get install xvfb
+# display = Xvfb()
+# display.start()
+# pip install selenium
+# pip install xvfbwrapper
