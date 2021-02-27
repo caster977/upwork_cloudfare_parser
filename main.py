0233e45c66988c51275e1703b134d3cb6b58b416
Pavel <caster977@gmail.com>
2021-02-27 18:51:41 +0200

    optimizing for chrome

--- main.py
+++ main.py
@@ -1,20 +1,41 @@
+from datetime import datetime
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 import urllib
 from xvfbwrapper import Xvfb
-
 import http.server
 import socketserver
 
-PORT = 8004
+PORT = 8003
 
 display = Xvfb()
 display.start()
-driver = webdriver.Chrome('drivers/chromedriver')
+chrome_options = webdriver.ChromeOptions()
+prefs = {
+    'profile.default_content_setting_values': {
+        'cookies': 2, 'images': 2, 'javascript': 2,
+        'plugins': 2, 'popups': 2, 'geolocation': 2,
+        'notifications': 2, 'auto_select_certificate': 2, 'fullscreen': 2,
+        'mouselock': 2, 'mixed_script': 2, 'media_stream': 2,
+        'media_stream_mic': 2, 'media_stream_camera': 2,
+        'protocol_handlers': 2,
+        'ppapi_broker': 2, 'automatic_downloads': 2, 'midi_sysex': 2,
+        'push_messaging': 2, 'ssl_cert_decisions': 2,
+        'metro_switch_to_desktop': 2,
+        'protected_media_identifier': 2, 'app_banner': 2,
+        'site_engagement': 2,
+        'durable_storage': 2
+    }
+}
+chrome_options.add_experimental_option("prefs", prefs)
+chrome_options.add_argument("start-maximized")
+chrome_options.add_argument("disable-infobars")
+chrome_options.add_argument("--disable-extensions")
+driver = webdriver.Chrome('drivers/chromedriver', options=chrome_options)
 
 
 class MyServer(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
+        start = datetime.now()
         driver.delete_all_cookies()
         parsed_path = urllib.parse.urlsplit(self.path)
         query = urllib.parse.parse_qs(parsed_path.query)
@@ -26,6 +47,7 @@ class MyServer(http.server.SimpleHTTPRequestHandler):
         self.end_headers()
 
         self.wfile.write(bytes(source, "utf8"))
+        print(datetime.now() - start)
 
 
 Handler = MyServer
